145b690743995cb6f7b05ec53ad7c7e6151eb7db
Hannah Joynson <hannahjoynson@gmail.com>
2022-05-11 12:41:12 +0100

    button changes

--- src/Components/Calculator.js
+++ src/Components/Calculator.js
@@ -5,46 +5,48 @@ const Calculator = () => {
     return (
         <div className="container">
             <div className="row justify-content-center">
-                <div className="col-md-4">
+                <div className="col-md-4 shadow-1g p-3 mb-5 mt-5 bg-white rounded;">
             <h1> React Calculator</h1>
-
-            <table>
+                <table className="table">
                 <tr>
-                    <td>
-                        <input type="text" />
+                    <td colSpan="3">
+                        <input className="w-100" type="text" />
                     </td>       
                     <td>
+
+{/* Buttons for Calculator */}
+
                     <button>C</button>
                     </td>
                  </tr>
+ 
+                 <tr>
+                 <td><button>1</button></td>
+                 <td><button>2</button></td>
+                 <td><button>3</button></td>
+                 <td><button>/</button></td>
+                </tr>
+
+                <tr>
+                 <td><button>4</button></td>
+                 <td><button>5</button></td>
+                 <td><button>6</button></td>
+                 <td><button>-</button></td>
+                </tr>
+
+                <tr>
+                 <td><button>7</button></td>
+                 <td><button>8</button></td>
+                 <td><button>9</button></td>
+                 <td><button>+</button></td>
+                </tr>
 
                  <tr>
-                 <button>1</button>
-                 <button>2</button>
-                 <button>3</button>
-                 <button>/</button>
-                </tr>
-
-                <tr>
-                 <button>4</button>
-                 <button>5</button>
-                 <button>6</button>
-                 <button>-</button>
-                </tr>
-
-                <tr>
-                 <button>7</button>
-                 <button>8</button>
-                 <button>9</button>
-                 <button>+</button>
-                </tr>
-
-                <tr>
-                 <button>.</button>
-                 <button>0</button>
-                 <button>=</button>
-                 <button>*</button>
-                </tr>
+                 <td><button>.</button></td>
+                 <td><button>0</button></td>
+                 <td><button>=</button></td>
+                 <td><button>*</button></td>
+                 </tr>
 
             </table>
             </div>
