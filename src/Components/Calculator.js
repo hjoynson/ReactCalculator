e7fcdb61250fc25bc1a91c0dc5eadac8e8202cd9
Hannah Joynson <hannahjoynson@gmail.com>
2022-05-11 12:06:33 +0100

    background color added to app

---
+++ src/Components/Calculator.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import "./Calculator.css";
+
+const Calculator = () => {
+    return (
+        <div>
+            <h1> React Calculator from Component</h1>
+        </div>
+    );
+};
+
+export default Calculator;
